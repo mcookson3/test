0224934978adbb68f283773c4f301b81bce9cf27
mcookson3 <73563419+mcookson3@users.noreply.github.com>
2020-10-27 17:17:31 -0600

    Add files via upload

---
+++ xtimeprerun.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 44.233.212.119 pt3.asrpro.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
